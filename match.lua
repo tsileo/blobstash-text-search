2b64bf5bb609d0249cb980225f8b7698d765c1ce
Thomas Sileo <thomas.sileo@gmail.com>
2016-09-25 18:49:58 +0200

    Tweaked search, working stem search, lot of debug print

--- match.lua
+++ match.lua
@@ -4,6 +4,13 @@
 --  print("match")
 --  return true
 -- end
+local stems = {}
+for ifield,field in ipairs(query.fields) do
+  if doc[field] ~= nil then
+    stems[field] = porterstemmer(doc[field])
+  end
+end
+
 
 print('doc=')
 for key,value in pairs(doc) do
@@ -12,29 +19,77 @@ end
 print('/doc')
 
 local ok = true
+match = 0
+
+
+function update_res (ok, prefix, cond)
+  print('update_res')
+  print(ok)
+  print(prefix)
+  print(cond)
+  print('/')
+  if prefix == '+' then
+    if cond then match = match + 1 end
+    print('prefix+')
+    print(ok and cond)
+    return ok and cond
+  end
+
+  if prefix == '-' then
+    if not cond then match = match + 1 end
+    print('prefix-')
+    print(ok and not cond)
+    return ok and not cond
+  end
+
+  print('default')
+  if cond then match = match + 1 end
+  print(ok or cond)
+  return ok or cond
+end
 
 print('iterquery=')
-for index,value in ipairs(query) do
+for index,value in ipairs(query.qs) do
   print(index,value)
   local prefix = ''
-  if string.sub(value, 1, 1) then
+  if string.sub(value, 1, 1) == '+' or string.sub(value, 1, 1) == '-' then
     prefix = string.sub(value, 1, 1)
     value = string.sub(value, 2, string.len(value))
   end
-  local b
   if string.sub(value, 1, 1) == '"' then
-    b = contains(string.sub(value, 2, string.len(value) - 1), doc.ok)
+    for qfield,field in ipairs(query.fields) do
+      if doc[field] ~= nil then
+        cond = false
+        if string.find(doc[field], string.sub(value, 2, string.len(value) - 1)) ~= nil then
+          cond = true
+        end
+        print('/')
+        print(field)
+        print(value)
+        print(cond)
+        print('/')
+        ok = update_res(ok, prefix, cond)
+      end
+    end
   else
     -- TODO the porter stemmer thing
+    for field,cstems in pairs(stems) do
+      cond = false
+      if cstems[value] ~= nil then
+        cond = true
+      end
+      print('/stem')
+      print(field)
+      print(value)
+      print(cond)
+      print('/stem')
+      ok = update_res(ok, prefix, cond)
+    end
   end
+end
 
-  if prefix == '' then
-    ok = ok or b
-  elseif prefix == '+' then
-    ok = ok and b
-  elseif preifx == '-' then
-    ok = ok and not b
-  end
+if ok and match == 0 then
+  return false
 end
 
 return ok
--- prequery.lua
+++ prequery.lua
@@ -7,59 +7,78 @@ function cstem (s)
     f = ''
   end
   if string.sub(s, 1, 1) == '"' and string.sub(s, string.len(s) - 1, 1) == '"' then
-    return f..porterstemmer_stem(s)
+    -- If the item is quoted, return it as it.
+    return f..s
   end
-  return f..s
-end
-function split_qs (s)
-local start = 0
-local out = ''
-local quoted = false
 
-local t = {}                   -- table to store the indices
-    local i = 0
-    while true do
-      -- for index,value in ipairs(t) do print(index,value) end
-      i = string.find(s, " ", start)    -- find 'next' newline
-      if i == nil then
-          if string.len(s) - start > 0 then
-             table.insert(t, cstem(string.sub(s, start, string.len(s))))
-          end
-          break
+  -- Lemmatize the word by default
+  return f..porterstemmer_stem(s)
+end
+
+function split_qs (s)
+  local start = 0
+  local out = ''
+  local quoted = false
+
+  local t = {}                   -- table to store the indices
+  local i = 0
+
+  while true do
+    -- for index,value in ipairs(t) do print(index,value) end
+    i = string.find(s, " ", start)    -- find 'next' separator
+    if i == nil then
+      if string.len(s) - start > 0 then
+        -- Insert the whole end of the string
+        table.insert(t, cstem(string.sub(s, start, string.len(s))))
       end
-      local sub = string.sub(s, start, i)
-      local cquote = false
-      if string.find(sub, '"', 0) then cquote = true end
-        if sub ~= " " then
-          if cquote then
-            if quoted then
-              out = out..sub
-              table.insert(t, cstem(out))
-              quoted = false
-              out = ''
-            else
-               quoted = true
-               out = out..sub
-            end
-          else
-            if quoted then
-              out = out..sub
-            else
-              table.insert(t, cstem(sub))
-            end
-          end
-        end
-      start = i+1
+      break
     end
- -- for index,value in ipairs(t) do print(index,value) end
-if out ~= "" then
-  table.insert(t, cstem(out))
+
+    -- Get the substring till the next space
+    local sub = string.sub(s, start, i)
+
+    -- Check if the substring is quoted
+    local cquote = false
+    if string.find(sub, '"', 0) then
+      cquote = true
+    end
+
+    if sub ~= " " then
+      if cquote then
+        -- The substring start with a quote
+        if quoted then
+          -- And there's an incomplete quoted string buffer
+          out = out..sub
+          -- We can complete it and reset the quote state
+          table.insert(t, cstem(out))
+          quoted = false
+          out = ''
+        else
+           -- Start a quote buffer
+           quoted = true
+           out = out..sub
+        end
+      else
+        if quoted then
+          -- We're still inside a quoted item, add the substring to the buffer
+          out = out..sub
+        else
+          -- It's a normal query item, add it to the list
+          table.insert(t, cstem(sub))
+        end
+      end
+    end
+    start = i+1
+  end
+   -- for index,value in ipairs(t) do print(index,value) end
+  if out ~= "" then
+    -- There still some data in the buffer, append it to the query (unquoted data?)
+    table.insert(t, cstem(out))
+  end
+  return t
 end
-return t
-end
-return split_qs(query)
+print(query)
+return {qs = split_qs(query.qs), fields = query.fields}
 
 -- keywords2 = keywords.match(/[-\+\:\w]+|["|'](?:\\"|[^i"])+['"]/g);
 -- ["okok", "+ok", "-yes", ""ook okokko k"", "'okok  lol'", "is:archived"]
-
-
